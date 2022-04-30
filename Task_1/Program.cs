a762bf935db13aa16ac65af145f9d91dbcca5981
ELIZA <rysaeva96@mail.ru>
2022-04-30 17:00:17 +0500

    Решение 4 задачи

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,16 +1,24 @@
 ﻿// Напишите программу, которая на вход принимает 
-// число и выдает, является ли число чётным 
-// (делится ли оно на два без остатка)
+// число (N), а выход показывает все четные числа
+// от 1 до N
 
-double num = double.Parse(Console.ReadLine());
-double result = num % 2;
-Console.WriteLine(result);
-if (result > 0)
+Console.Write("Введите целое число = ");
+int n = int.Parse(Console.ReadLine());
+for(int i = 1; i <= n; i++)
 {
-    Console.WriteLine("Число " + num + " является нечетным");
+    Console.WriteLine(i + " ");
+}
+for(int i = 1; i <= n-1; i++)
+{
+  int num = i % 2;
+  if (num < 0)
+  {
+      Console.Write(i);
+  }
+  else
+  {
+      i++;
+  }
+  Console.Write(i + " ");
 }
 
-else
-{
-    Console.WriteLine("Число " + num + " является четным");
-}
