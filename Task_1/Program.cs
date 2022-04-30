8d16cb6f97b5c61cc13e6f8b7d5f800a0aacd884
ELIZA <rysaeva96@mail.ru>
2022-04-30 17:03:12 +0500

    Решение 4 задачи

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,24 @@
-﻿
+﻿// Напишите программу, которая на вход принимает 
+// число (N), а выход показывает все четные числа
+// от 1 до N
+
+Console.Write("Введите целое число = ");
+int n = int.Parse(Console.ReadLine());
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine(i + " ");
+}
+for(int i = 1; i <= n-1; i++)
+{
+  int num = i % 2;
+  if (num < 0)
+  {
+      Console.Write(i);
+  }
+  else
+  {
+      i++;
+  }
+  Console.Write(i + " ");
+}
 
