499a5c352c5c6b4ad9191318a0239e0b75783008
ELIZA <rysaeva96@mail.ru>
2022-04-30 15:15:44 +0500

    empty

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,16 +1,1 @@
-﻿// Напишите программу, которая на вход принимает два числа
-// и выдает, какое число большее, а какое меньшее
-
-int numberA = int.Parse(Console.ReadLine());
-int numberB = int.Parse(Console.ReadLine());
-if (numberA > numberB)
-{
-   Console.WriteLine("Большее число: " + numberA);
-   Console.WriteLine("Меньшее число: " + numberB);
-   
-}
-else
-{
-   Console.WriteLine("Большее число: " + numberB);
-   Console.WriteLine("Меньшее число: " + numberA); 
-}
+﻿
